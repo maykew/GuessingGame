304f70bcbe32998296b139e50e1c813f53d60d81
Mayke Willians <maykewillians3@gmail.com>
2020-07-05 10:24:11 -0300

    Add comments servidor and cliente

--- cliente.py
+++ cliente.py
@@ -3,11 +3,19 @@ import struct
 
 HOST = '127.0.0.1'     # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
-# Campos da mensagem
-idSensor = 70000
-tpSensor = 50000
-vlSensor = -169
 
+# Campos da mensagem
+#idSensor = 70000
+#tpSensor = 50000
+#vlSensor = -169
+
+#-------------- Funcoes --------------
+
+# Funcao de empacotamento e envio de da mensagem
+#def mySend (socket, idSensor, tpSensor, vlSensor):
+#	msg = struct.pack('!IHh', idSensor, tpSensor, vlSensor) # Empacota a informação
+#	print (msg)
+#	socket.send (msg)
 
 #Funcao que recebe uma mensagem e uma conexao como paramentro, e encaminha a mensagem para o cliente
 def enviaMensagem(msg, destino):
@@ -20,26 +28,27 @@ def recebeMensagem(remetente):
     msg = msg.decode('UTF-8')	# Decodifica a mensagem
     return msg
 
-# Funcao de empacotamento e envio de da mensagem
-#def mySend (socket, idSensor, tpSensor, vlSensor):
-#	msg = struct.pack('!IHh', idSensor, tpSensor, vlSensor) # Empacota a informação
-#	print (msg)
-#	socket.send (msg)
 
-######### Inicio do programa	
+
+#-------------- Inicio do programa --------------
+
+# Cria o socket do cliente
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
+dest = (HOST, PORT)  # Forma a tupla de host(ip), porta
+tcp.connect(dest)  # Estabelece a conexao
 
+# Tenta se conectar ao servidor
 try:
     #Recebendo mensagem inicial
     msg = recebeMensagem(tcp)
     print (msg)
 
+# Caso não consiga se conectar é obtido um erro
 except:
     print("\nO jogo foi iniciado antes de você conseguir se conectar, tente novamente")
     tcp.close()
 
+#Caso consiga se conectar, o programa segue normalmente
 else:
     #Recebendo mensagem da jogada
     msg = recebeMensagem(tcp)
@@ -48,7 +57,7 @@ else:
     jog = input(msg)
     enviaMensagem(jog, tcp)
 
-    #Recebendo mensagem inicial
+    #Recebendo mensagem de resultado
     msg = recebeMensagem(tcp)
     result = msg.split(",") 
     print ("\nNumero sorteado:",result[0],"\nPosição final: ", result[1])
@@ -56,4 +65,4 @@ else:
     ######### Envia uma mensagem
     #mySend (tcp, idSensor, tpSensor, vlSensor)
 
-    tcp.close()
+    tcp.close()  # Fecha a conexao com o servidor
--- servidor.py
+++ servidor.py
@@ -3,6 +3,7 @@ import struct
 import _thread
 import time
 import random
+
 HOST = ''              # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 
@@ -11,6 +12,17 @@ PORT = 5000            # Porta que o Servidor esta
 #tpSensor = None
 #vlSensor = None
 #structsize = 8
+
+#-------------- Funcoes --------------
+
+# Funcao de recepcao e desempacotamento da mensagem
+#def myRecv (socket):
+#	try:	
+#		msg = socket.recv(structsize)
+#		print (cliente, msg)
+#		return struct.unpack('!IHh', msg) # Desempacota a informação
+#	except:
+#		return None
 
 #Funcao que recebe uma mensagem e uma conexao como paramentro, e encaminha a mensagem para o cliente
 def enviaMensagem(msg, destino):
@@ -23,15 +35,7 @@ def recebeMensagem(remetente):
     msg = msg.decode('UTF-8')	# Decodifica a mensagem
     return msg
 
-# Funcao de recepcao e desempacotamento da mensagem
-#def myRecv (socket):
-#	try:	
-#		msg = socket.recv(structsize)
-#		print (cliente, msg)
-#		return struct.unpack('!IHh', msg) # Desempacota a informação
-#	except:
-#		return None
-	
+#Funcao que abre a conexao com um cliente
 def conectado(con, cliente):
     #idSensor, tpSensor, vlSensor = myRecv(con)
     #print (cliente, idSensor, tpSensor, vlSensor)
@@ -39,16 +43,19 @@ def conectado(con, cliente):
     enviaMensagem("\nHi! Welcome to Guessing Game\n", con)
     _thread.exit()
 
+#Funcao que encerra a conexao com um cliente
 def finaliza(con, cliente):
     print ('Finalizando conexao do cliente', cliente)
     con.close()
 
+#Funcao que envia mensagem para receber jogada e recebe a jogada
 def setJogadas(con, cliente):
     enviaMensagem("\nQual a sua jogada: ", con)
     jogada = recebeMensagem(con)
     jogadas[con] = int(jogada)
     _thread.exit()
 
+#Funcao que ordena a lista de clientes com base na diferença entre a jogada e numero sorteado
 def ordenaLista():
     troca=True
     aux=0
@@ -63,47 +70,65 @@ def ordenaLista():
                 clientes[i+1]=aux
                 troca=True
 
+#Funcao que envia resultado aos clientes
 def resultado():
     for i in range(len(clientes)):
         msg = str(numero)+","+str(i+1)
         enviaMensagem(msg, clientes[i][0])
-    
 
+
+
+#-------------- Inicio do programa --------------    
+
+# Cria o socket do servidor
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(10)
+orig = (HOST, PORT)  # Forma a tupla de host e porta
+tcp.bind(orig)  # Solicita ao S.O. acesso exclusivo a porta
+tcp.listen(10)  # Entra no modo de escuta
 
+#Lista de clientes que armazenda conxecao e o cliente em uma tupla
 clientes = []
+
+#Dicionario de jogadas onde a chave é a conexao e o valor é a jogada 
 jogadas = {}
-aguardarJog = True
+
+aguardarJog = True  # Variavel para aguardar jogadores
 
 print("\nAguardando conexões...")
-while True:
+while aguardarJog:
+    #Realiza a conexao dos clientes
     con, cliente = tcp.accept()
     clientes.append((con, cliente))
+    
+    #Inicia uma thread para conectar o cliente
     _thread.start_new_thread(conectado, tuple([con, cliente]))
-    #print("")
+    
+    #Caso o servidor receba o valor de inicio do jogo(1), nao são ouvidas mais conexoes 
     if input() == '1': 
-        break
+        aguardarJog = False
 
-numero = random.randint(1, 100)
+numero = random.randint(1, 100)  # Armazena numero aleatorio de 1 a 100
 
+#Envia e recebe de jogadas e armazena no dicionario
 for cli in clientes:
     _thread.start_new_thread(setJogadas, cli)
 
+#Espera todos os jogadores enviarem as jogadas (todas as threads finalizarem)
 print ("\nAguardando jogadas...")
 while len(jogadas) != len(clientes):
     loopDeEspera = "Aguardando threads finalizarem"
 
+#Print para controle do servidor
 print("\nNumero sorteado: ", numero)
 
+#Print para controle do servidor
 print("\nJogadas:")
 for cli in clientes:
     print (cli[1],"- ",jogadas[cli[0]])
 
 ordenaLista()
 
+#Print para controle do servidor
 print("\nResultado final:")
 for i in range(len(clientes)):
     cli = clientes[i]
@@ -111,8 +136,10 @@ for i in range(len(clientes)):
 
 resultado()
 
+#Encerra as conexoes
 print("\n")
 for cli in clientes:
     finaliza(cli[0], cli[1])
 
+# Fecha a conexao
 tcp.close()
